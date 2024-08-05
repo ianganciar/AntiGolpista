ffab0ceac3026efc02abcab9343a0cc77900f9e8
ianganciar <ianganciar98@gmail.com>
2024-08-05 11:53:44 -0300

    refactor:  replace QueryCount with TrustedOccurrences in TrustedPhoneNumber entity

---
+++ AntiGolpista.Domain/Entities/TrustedOccurrence.cs
@@ -0,0 +1,7 @@
+﻿namespace AntiGolpista.Domain.Entities;
+public class TrustedOccurrence : BaseEntity
+{
+    public DateTime OccurrenceDate { get; private set; } = DateTime.Now;
+    public int TrustedPhoneNumberId { get; private set; }
+    public TrustedPhoneNumber? TrustedPhoneNumber { get; private set; }
+}
--- AntiGolpista.Domain/Entities/TrustedPhoneNumber.cs
+++ AntiGolpista.Domain/Entities/TrustedPhoneNumber.cs
@@ -2,7 +2,7 @@
 public class TrustedPhoneNumber(string phoneNumber)
 {
     public string PhoneNumber { get; private set; } = phoneNumber;
-    public int QueryCount { get; private set; }
+    public List<TrustedOccurrence> TrustedOccurrences { get; private set; } = [];
     public int CompanyId { get; private set; }
     public Company? Company { get; private set; }
 }
