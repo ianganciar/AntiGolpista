a09dfa65feea6c4bb9fb649df3a36b6121c003c9
ianganciar <ianganciar98@gmail.com>
2024-08-05 10:45:58 -0300

    Change the ValueObject Url to URI

--- AntiGolpista.Domain/Entities/Company.cs
+++ AntiGolpista.Domain/Entities/Company.cs
@@ -7,22 +7,22 @@ public class Company : BaseEntity
     public Name Name { get; private set; }
     public PhoneNumber SupportPhoneNumber { get; private set; }
     public Document Document { get; private set; }
-    public Url DocumentImageUrl { get; private set; }
-    public Url ProfileImageUrl { get; private set; }
-    public Url BannerImageUrl { get; private set; }
+    public Uri DocumentImageUrl { get; private set; }
+    public Uri ProfileImageUrl { get; private set; }
+    public Uri BannerImageUrl { get; private set; }
     public bool IsVerified { get; private set; } = false;
     public int UserId { get; private set; }
     public User? User { get; private set; }
     public List<TrustedPhoneNumber> TrustedPhonesNumbers { get; private set; } = [];
     public List<UntrustedPhoneNumber> UntrustedPhoneNumbers { get; private set; } = [];
-   
+
     public Company(
         Name name,
         PhoneNumber supportPhoneNumber,
         Document document,
-        Url documentImageUrl,
-        Url profileImageUrl,
-        Url bannerImageUrl,
+        Uri documentImageUrl,
+        Uri profileImageUrl,
+        Uri bannerImageUrl,
         IPasswordEncripter passwordEncripter)
     {
         Name = name;
@@ -49,17 +49,17 @@ public class Company : BaseEntity
         Document = newDocument;
     }
 
-    public void UpdateDocumentImageUrl(Url newDocumentImageUrl)
+    public void UpdateDocumentImageUrl(Uri newDocumentImageUrl)
     {
         DocumentImageUrl = newDocumentImageUrl;
     }
 
-    public void UpdateProfileImageUrl(Url newProfileImageUrl)
+    public void UpdateProfileImageUrl(Uri newProfileImageUrl)
     {
         ProfileImageUrl = newProfileImageUrl;
     }
 
-    public void UpdateBannerImageUrl(Url newBannerImageUrl)
+    public void UpdateBannerImageUrl(Uri newBannerImageUrl)
     {
         BannerImageUrl = newBannerImageUrl;
     }
