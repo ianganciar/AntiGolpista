ab46dcb7480029b5157434ae4aeed5fb098eff2f
ianganciar <ianganciar98@gmail.com>
2024-08-12 23:48:19 -0300

    refactor: interface IPasswordEncripter removida

--- AntiGolpista.Domain/Security/Cryptography/IPasswordEncripter.cs
+++
@@ -1,9 +0,0 @@
-﻿using AntiGolpista.Domain.ValueObjects;
-
-namespace AntiGolpista.Domain.Security.Cryptography;
-
-public interface IPasswordEncripter
-{
-    public string Encrypt(string password);
-    public bool IsInvalid(string password, string userPassoword);
-}
